c0f851ebe37620ed98ef23d07bfe6c8de51d97c7
Stephen Lucic M. <stephen.lucic.maldonado@gmail.com>
2025-11-12 01:57:57 -0300

    [build] Se agrega imagen de mysql en docker y se conecta django con este

--- Focusa/focusa/focusa/settings.py
+++ Focusa/focusa/focusa/settings.py
@@ -66,10 +66,24 @@ WSGI_APPLICATION = 'focusa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'focusa_bd',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3307',
+        'OPTIONS': {
+            'autocommit': True,   
+        }
     }
 }
 
